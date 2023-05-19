5627929887fc73bd1709ddb2cd0b9a2eabd22a5a
Terry Nguyen <terry@terrehbyte.com>
2023-05-19 12:27:51 -0700

    Recreate background and integrate UI SFX

---
+++ GodotTimer/Scenes/EvaTimerScreen.gd
@@ -0,0 +1,7 @@
+extends Control
+
+@export var fillControl : TextureRect
+@export var timer : TimerContainer
+
+func _on_timer_timer_second_tick(seconds_remaining):
+	pass # Replace with function body.
--- GodotTimer/Scenes/TimerContainer.gd
+++ GodotTimer/Scenes/TimerContainer.gd
@@ -1,22 +1,50 @@
 extends HBoxContainer
+class_name TimerContainer # declares this type globally
 
+# exports are visible to the inspector
 @export var mssNode : Label
 @export var nnNode : Label
-
 @export var totalSeconds : float
+@export var tickSound : AudioStream
+@export var sfxPlayer : AudioStreamPlayer
 
+# signals are events that other scripts can react to
+signal timer_started(seconds_to_complete)
+signal timer_completed
+signal timer_second_tick(seconds_remaining)
+
+# plain variables
+var isTimerCompleted : bool = false
+var lastSecondTick : int
 var remainingSeconds : float
 
+# Called at start of lifetime.
 func _ready():
+	timer_started.emit(remainingSeconds)
 	remainingSeconds = totalSeconds
+	lastSecondTick = remainingSeconds
+	timer_second_tick.emit(remainingSeconds)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+# Called every frame while alive. 'delta' is the elapsed time since the previous frame.
+func _process(delta : float):
 	remainingSeconds -= delta
 	remainingSeconds = max(remainingSeconds, 0)
 	var minutesLeft = int(remainingSeconds) / 60
 	var secondsLeft = int(remainingSeconds) % 60
 	var nanosecLeft = (remainingSeconds - int(remainingSeconds)) * 100
-	mssNode.text = str(minutesLeft).pad_zeros(0) + ":" + str(secondsLeft).pad_zeros(2)
+	mssNode.text = str(minutesLeft).pad_zeros(2) + ":" + str(secondsLeft).pad_zeros(2)
 	nnNode.text = ":" + str(int(nanosecLeft)).pad_decimals(0).pad_zeros(2)
+	
+	# Update tick signal if we've ticked over to the next second
+	if lastSecondTick != int(remainingSeconds):
+		lastSecondTick = remainingSeconds
+		timer_second_tick.emit(lastSecondTick)
+		sfxPlayer.stream = tickSound
+		sfxPlayer.play()
+	
+	# Emit completed signal if timer has finished counting down
+	if !isTimerCompleted && remainingSeconds == 0:
+		isTimerCompleted = true
+		timer_completed.emit()
+		
 	pass
